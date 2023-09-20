0b710e234f89824b43a8ecc73454a6bf5a9ef182
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-20 15:58:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,13 @@ from matplotlib import style
 def development(data): 
     data = pd.read_csv("a.csv")
     data.describe()
-    return data["Adolescent fertility rate' &&\
-    ' (births per 1,000 women ages 15-19)"].describe().loc['mean']
+    return data["a"].describe().loc['mean']
 
 # generate Plot
 
 
 def plot(data):
-    my_chart = (so.Plot(data, x="Mortality rate, infan' &&\
-    't (per 1,000 live births)", y="GDP per capita (constant 2010 US$)")
+    my_chart = (so.Plot(data, x="a")", y="GDP per capita (constant 2010 US$)")
     .add(so.Line(), so.PolyFit(order=2))
     .add(so.Dot())
     .label(title="Log GDP and infat Mortality")
