00d75f45a46779e2b981121e24a6a960fdee9ce7
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-19 21:45:21 -0400

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -12,14 +12,14 @@ from lib import plot
 
 def test_data():
     # Test with dataset
-    data= pd.read_csv("https://media.githubusercontent.com/media/
-    nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
+    data= pd.read_csv("https://media.githubusercontent.com/media/'\
+    'nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
     result = development(data)
     assert result == 47.790116494845364
 
 def test_plot(data):
-    data= pd.read_csv("https://media.githubusercontent.com/media/nickeubank/
-    MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
+    data= pd.read_csv("https://media.githubusercontent.com/media/nickeubank/'\
+    'MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
     my_chart = (so.Plot(data, x="Mortality rate, infant (per 1,000 live births)", 
                         y="GDP per capita (constant 2010 US$)")
     .add(so.Line(), so.PolyFit(order=2))
