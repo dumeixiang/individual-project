22cc3d5c274eb4ff21e4b3a19824d96db2363cda
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-20 21:06:20 -0400

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -5,14 +5,22 @@ Test goes here
 
 import pandas as pd
 from lib import development
-#from lib import plot
+from lib import plot
 
 def test_data():
     # Test with dataset
     data= pd.read_csv("a.csv")
     result = development(data)
     assert result == 47.790116494845364
+    
+def test_plot(path):
+    #data= pd.read_csv(path)
+    #plot(data)
+   # result = 1
+    #fig = plt.gcf()
+    #assert len(fig.axes) > 0
+    assert result == 1
 
 if __name__ == "__main__":
     test_data()
-   # test_plot("./a.csv")
+    test_plot("./a.csv")
