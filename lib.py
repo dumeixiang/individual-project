5e9bb3d35a652df41baf4e14c68dfdf5e99c823b
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-20 23:23:39 -0400

    Update lib.py

--- lib.py
+++ lib.py
@@ -2,9 +2,9 @@
 Main code
 """
 import pandas as pd
-#import seaborn.objects as so
-#from matplotlib import style
-#import numpy as np
+import seaborn.objects as so
+from matplotlib import style
+import numpy as np
 
 
 # Download World Development Indicators
@@ -14,6 +14,14 @@ def development(data):
     return data["a"].describe().loc['mean']
 
 # generate Plot
+def plot(data):
+    my_chart = (so.Plot(data, x="Mortality rate, infant (per 1,000 live births)", 
+                        y="GDP per capita (constant 2010 US$)")
+    .add(so.Line(), so.PolyFit(order=2))
+    .add(so.Dot())
+    .label(title="Log GDP and infat Mortality")
+    .theme({**style.library["seaborn-whitegrid"]}))
+    my_chart.show()
 
 
 
