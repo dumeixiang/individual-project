4f418d18d6272d0bf4965addad1918384e9311e1
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-20 21:09:10 -0400

    Update lib.py

--- lib.py
+++ lib.py
@@ -2,8 +2,8 @@
 Main code
 """
 import pandas as pd
-import seaborn.objects as so
-from matplotlib import style
+#import seaborn.objects as so
+#from matplotlib import style
 #import numpy as np
 
 
@@ -16,11 +16,4 @@ def development(data):
 # generate Plot
 
 
-def plot(data):
-    my_chart = (so.Plot(data, x="Mortality rate, infant (per 1,000 live births)", 
-                        y="GDP per capita (constant 2010 US$)")
-    .add(so.Line(), so.PolyFit(order=2))
-    .add(so.Dot())
-    .label(title="Log GDP and infat Mortality")
-    .theme({**style.library["seaborn-whitegrid"]}))
-    my_chart.show()
+
